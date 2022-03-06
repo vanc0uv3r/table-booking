6569e95c102e272f5b2c54e84e8eaaff5f79cc2f
Ivan Gorsky <lesaltermans@gmail.com>
2022-03-06 15:22:03 +0300

    Change structs: tuple of time and list, where time is key

--- src/Booking.hs
+++ src/Booking.hs
@@ -12,27 +12,21 @@ import Data.Fixed
 oneM = 60
 oneH = oneM * 60
 
-type Tables = [Table]
+type Tables = [(UTCTime, [TableState])]
 
-data TableState = TableState {time :: UTCTime,
-                              isFree :: Bool,
+data TableState = TableState {isFree :: Bool,
                               name :: String,
                               phone :: String,
                               persons :: Int 
 } deriving Show
 
 data DayL = DayL {date :: UTCTime,
-                tables :: [[Table]],
+                tables :: Tables,
                 openT :: UTCTime,
                 closeT :: UTCTime,
                 interval :: Int,
                 tableNum :: Int
 } deriving Show
-
-
-data Table = Table {id :: Int, 
-                    times :: [TableState]
-                    } deriving Show
 
 
 mkUTCTime :: (Integer, Int, Int)
@@ -52,32 +46,26 @@ initDay currDay openT closeT interval numT =
     DayL currDay [] openT closeT interval numT
 
 
-initTable :: UTCTime -> 
+initTimes :: UTCTime -> 
              UTCTime -> 
-             [Table] ->
+             Tables ->
              Int -> 
              Int -> 
-             [Table]    
-initTable _ _ _ _ 0 = []
-initTable openT closeT tables interval numT = 
-    table : initTable openT closeT tables interval (numT-1)
-    where 
-        table = Table numT times 
-        times = initTimes openT closeT [] interval 
-      
-
-initTimes :: UTCTime ->
-             UTCTime ->
-             [TableState] ->
-             Int ->
-             [TableState]
-initTimes openT closeT tables interval = 
+             Tables    
+initTimes openT closeT times interval numT = 
     if diffUTCTime closeT openT >= realToFrac interval then
-        table : initTimes newOpenT closeT tables interval 
+        time : initTimes newOpenT closeT times interval numT 
     else []
-    where 
+    where
+        time = (openT, tables)
+        tables = initTables numT 
         newOpenT = addUTCTime (realToFrac interval) openT
-        table = TableState openT False "" "" 0 
+
+initTables :: Int -> [TableState]
+initTables 0 = [] 
+initTables n = table : initTables (n-1) where
+    table = TableState True "" "" 0 
+
 
 book :: IO()
 book = do  
@@ -85,10 +73,11 @@ book = do
     let currDay = toGregorian $ utctDay currTime 
     let openT = mkUTCTime currDay (10, 0, 0)
     let closeT = mkUTCTime currDay (22, 0, 0)
-    let times = initTimes openT closeT [] (oneM*15) 
-    let tables = initTable openT closeT [] (oneM*15) 5
+    let dayS = initTimes openT closeT [] 900 5
     let newDay = initDay currTime openT closeT 15 5 
-    putStrLn . show $ tables 
+    let res = snd (dayS !! 1)
+    let fi = filter(\x -> isFree x == False) res
+    putStrLn . show $ fi 
 
 
 
